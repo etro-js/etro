b8966a8dca13fe33ccbd75ea1279b479cb43b087
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2019-10-08 23:21:58 -0400

    :wrench: Rename test -> spec
    Edit karma config file to point to new location

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'dist/vidar-iife.js',
-      'test/*.spec.js'
+      'spec/*.spec.js'
     ],
 
 
