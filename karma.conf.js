1485cf2c91ec817a23bad64c1071005c80dd0c11
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2019-10-18 18:02:24 -0400

    :wrench: Update production file in karma config
    dist/vidar-iife.js -> dist/vidar.js

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'dist/vidar-iife.js',
+      'dist/vidar.js',
       'spec/*.spec.js'
     ],
 
