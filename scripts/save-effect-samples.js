56b10f95ad9005d76e0344a42df86f9a162b0047
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2019-11-13 11:37:42 -0500

    :white_check_mark: Add 'effects' script
    Used for generating before/after images for unit testing effects

---
+++ scripts/save-effect-samples.js
@@ -0,0 +1,43 @@
+/**
+ * @module
+ * <em>Run this script and review the results whenever you add a unit test or make an effect algorithm change.</em>
+ */
+
+const path = require('path')
+const fs = require('fs')
+const puppeteer = require('puppeteer')
+
+// parent directory
+const projectDir = __dirname.split('/').slice(0, -1).join('/')
+
+function createDirs(filePath) {
+  const dirname = path.dirname(filePath)
+  if (fs.existsSync(dirname)) {
+    return
+  }
+  createDirs(dirname)
+  fs.mkdirSync(dirname)
+}
+
+(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  page.on('load', async () => {
+    // console.log(await page.$$('p'));
+    const items = await page.$$eval('p', elems => elems.map(p => {
+      return { data: p.innerHTML, path: p.dataset.path }
+    }))
+
+    items.forEach(item => {
+      // remove prefix and save to png
+      const buffer = Buffer.from(item.data.replace(/^data:image\/png;base64,/, ''), 'base64')
+      console.log(`writing ${item.path} ...`)
+      const path = projectDir + '/spec/assets/effect/' + item.path
+      createDirs(path)
+      fs.writeFileSync(path, buffer)
+    })
+    await browser.close()
+  })
+  await page.goto(`file://${__dirname}/gen-effect-samples.html`)
+})()
