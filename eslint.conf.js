3f6600d674270545a1575220f5423a8bd8e80f97
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2019-11-19 22:58:03 -0500

    :rotating_light: Lint js config files

--- eslint.conf.js
+++ eslint.conf.js
@@ -1,28 +1,28 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es6': true
-    },
-    'extends': [
-        'standard'
-    ],
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
-    },
-    'parserOptions': {
-        'ecmaVersion': 2018,
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'html'
-    ],
-    'rules': {
-        'brace-style': ['error', '1tbs', { 'allowSingleLine': false }]
-    },
-    'settings': {
-      'html': {
-        'indent': '+2'
-      }
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'html'
+  ],
+  rules: {
+    'brace-style': ['error', '1tbs', { allowSingleLine: false }]
+  },
+  settings: {
+    html: {
+      indent: '+2'
     }
+  }
 }
