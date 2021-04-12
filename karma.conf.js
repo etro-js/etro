27b8b0c29019ffff5b0c9f3ffd5bf04fb27f930a
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2021-04-12 18:13:39 -0400

    :wrench: Use es6-shim in karma

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function (config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'requirejs'],
+    frameworks: ['jasmine', 'requirejs', 'es6-shim'],
 
     // list of files / patterns to load in the browser
     files: [
