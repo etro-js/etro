vantezzen/mular-renderer

:sparkles: Add BaseAudio layer
- Add BaseAudioMixin and BaseAudio layers
- Make AudioSourceMixin inherit from BaseAudioSourceMixin

Resolves #67

spec/layer.spec.js
@@ -273,7 +273,7 @@ describe('Layers', function () {
   describe('AudioSource', function () {
     let layer
     // Media is an abstract mixin, so make a concrete subclass here.
-    const CustomMedia = vd.layer.AudioSourceMixin(vd.layer.Base)
+    const CustomMedia = vd.layer.AudioSourceMixin(vd.layer.BaseAudio)
     let source
 
     beforeAll(function (done) {

etro-js/etro

spec/layer.spec.js
@@ -311,7 +311,7 @@ describe('Layers', function () {
   describe('AudioSource', function () {
     let layer
     // Media is an abstract mixin, so make a concrete subclass here.
-    const CustomMedia = etro.layer.AudioSourceMixin(etro.layer.Base)
+    const CustomMedia = etro.layer.AudioSourceMixin(etro.layer.BaseAudio)
     let source
 
     beforeAll(async function () {