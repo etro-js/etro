0ddf94a81a4a6a0991f69ddd2aa9f2f662c34eac
Shauna Empringham <shaunathedead86@gmail.com>
2022-07-02 11:06:33 -0400

    :bug: Fix image options type #116

--- dist/layer/image.d.ts
+++ dist/layer/image.d.ts
@@ -1,5 +1,5 @@
-import { VisualOptions } from './visual';
-declare type ImageOptions = VisualOptions;
+import { VisualSourceOptions } from './visual-source';
+declare type ImageOptions = VisualSourceOptions;
 declare const Image_base: new (...args: unknown[]) => import("./visual-source").VisualSource;
 declare class Image extends Image_base {
 }
