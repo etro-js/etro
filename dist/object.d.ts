9c17a3de4f0ca9bf6c0145c6315bdfd8d3a8dd77
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2022-09-16 17:33:45 -0400

    :wastebasket: Deprecate `publicExcludes`

--- dist/object.d.ts
+++ dist/object.d.ts
@@ -3,7 +3,11 @@ import { Movie } from './movie';
 export default interface EtroObject {
     /** Used in etro internals */
     type: string;
-    /** Which properties to not watch for changes, for `Movie#autoRefresh` */
+    /**
+     * Which properties to not watch for changes, for `Movie#autoRefresh`
+     *
+     * @deprecated `Movie#autoRefresh` is deprecated
+     */
     publicExcludes: string[];
     /** Map of property name to function to run on result of `val` */
     propertyFilters: Record<string, <T>(value: T) => T>;
