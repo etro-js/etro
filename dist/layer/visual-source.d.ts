4d2a204ce59e9cbfed517e092b380bed82493c4f
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2022-09-17 12:39:50 -0400

    :label: Add missing properties to `VisualSource` interface

--- dist/layer/visual-source.d.ts
+++ dist/layer/visual-source.d.ts
@@ -3,6 +3,22 @@ import { Visual, VisualOptions } from './visual';
 declare type Constructor<T> = new (...args: unknown[]) => T;
 interface VisualSource extends Visual {
     readonly source: HTMLImageElement | HTMLVideoElement;
+    /** What part of {@link source} to render */
+    sourceX: Dynamic<number>;
+    /** What part of {@link source} to render */
+    sourceY: Dynamic<number>;
+    /** What part of {@link source} to render, or undefined for the entire width */
+    sourceWidth: Dynamic<number>;
+    /** What part of {@link source} to render, or undefined for the entire height */
+    sourceHeight: Dynamic<number>;
+    /** Where to render {@link source} onto the layer */
+    destX: Dynamic<number>;
+    /** Where to render {@link source} onto the layer */
+    destY: Dynamic<number>;
+    /** Where to render {@link source} onto the layer, or undefined to fill the layer's width */
+    destWidth: Dynamic<number>;
+    /** Where to render {@link source} onto the layer, or undefined to fill the layer's height */
+    destHeight: Dynamic<number>;
 }
 interface VisualSourceOptions extends VisualOptions {
     source: HTMLImageElement | HTMLVideoElement;
