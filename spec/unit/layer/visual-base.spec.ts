f163f750608cf42ff36400df7d5800e6afbea4f2
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2022-10-23 11:07:59 -0400

    :white_check_mark: Unit test VisualBase with views

--- spec/unit/layer/visual-base.spec.ts
+++ spec/unit/layer/visual-base.spec.ts
@@ -1,59 +1,77 @@
 import etro from '../../../src/index'
+import { mockDOMView } from '../mocks/dom-view'
 import { mockMovie } from '../mocks/movie'
 
-describe('Unit Tests ->', function () {
-  describe('Layers ->', function () {
-    describe('VisualBase ->', function () {
-      class CustomVisualBase extends etro.layer.VisualBase {
-        doRender (): void {
-          // Do nothing
+const runConfigs = [
+  {
+    useView: true
+  },
+  {
+    useView: false
+  }
+]
+
+runConfigs.forEach(runConfig => {
+  describe('Unit Tests ->', function () {
+    describe('Layers ->', function () {
+      describe(`VisualBase (${runConfig.useView ? 'view' : 'canvas'}) ->`, function () {
+        class CustomVisualBase extends etro.layer.VisualBase {
+          doRender (): void {
+            // Do nothing
+          }
         }
-      }
 
-      let layer: etro.layer.VisualBase
+        let layer: etro.layer.VisualBase
 
-      beforeEach(function () {
-        layer = new CustomVisualBase({
-          startTime: 0,
-          duration: 4
+        beforeEach(function () {
+          layer = new CustomVisualBase({
+            startTime: 0,
+            duration: 4,
+            view: runConfig.useView ? mockDOMView() : undefined
+          })
         })
-      })
 
-      it('should have no view if useView is not provided in the constructor', function () {
-        expect(layer.view).toBeNull()
-      })
+        if (runConfig.useView)
+          it('should have a view', function () {
+            expect(layer.view).toBeDefined()
+          })
+        else
+          it('should have no view', function () {
+            expect(layer.view).toBeUndefined()
+          })
 
-      it('should be able to render after an effect has been directly deleted', function () {
-        const movie = mockMovie()
-        layer.attach(movie)
-        layer.render()
-        // Clear cache populated by render()
-        etro.clearCachedValues(movie)
+        it('should be able to render after an effect has been directly deleted', function () {
+          const movie = mockMovie()
+          layer.attach(movie)
+          layer.render()
+          // Clear cache populated by render()
+          etro.clearCachedValues(movie)
 
-        // Start with one effect
-        layer.addEffect(jasmine.createSpyObj('effect1', ['apply', 'tryAttach', 'tryDetach']))
+          // Start with one effect
+          layer.addEffect(jasmine.createSpyObj('effect1', ['apply', 'tryAttach', 'tryDetach']))
 
-        // Delete the effect
-        delete layer.effects[0]
+          // Delete the effect
+          delete layer.effects[0]
 
-        // Render
-        layer.render()
-      })
+          // Render
+          layer.render()
+        })
 
-      it('should not call doRender if it has an empty canvas', function () {
-        const movie = mockMovie()
-        layer.attach(movie)
-        layer.render()
-        // Clear cache populated by render()
-        etro.clearCachedValues(movie)
+        it('should not call doRender if it has an empty canvas', function () {
+          const movie = mockMovie()
+          layer.attach(movie)
+          layer.render()
+          // Clear cache populated by render()
+          etro.clearCachedValues(movie)
 
-        layer.width = 0
-        layer.height = 0
-        spyOn(layer, 'doRender')
+          layer.width = 0
+          layer.height = 0
+          spyOn(layer, 'doRender')
 
-        layer.render()
+          layer.render()
 
-        expect(layer.doRender).toHaveBeenCalledTimes(0)
+          expect(layer.doRender).toHaveBeenCalledTimes(0)
+        })
       })
     })
   })
