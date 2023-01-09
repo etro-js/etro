etro-js/etro

commit ac3322b57fb149a5175ddbe10c0207e7835cc042

:bug: Give `duration` option a default value
Closes #164

src/layer/audio-source.ts
@@ -69,7 +69,13 @@ function AudioSourceMixin<OptionsSuperclass extends BaseOptions> (superclass: Co
       const onload = options.onload
       // Don't set as instance property
       delete options.onload
+
+      // Set a default duration so that the super constructor doesn't throw an
+      // error
+      options.duration = options.duration || 0
+
       super(options)
+
       this._initialized = false
       this._sourceStartTime = options.sourceStartTime || 0
       applyOptions(options, this)

spec/unit/layer/audio-source.spec.ts
@@ -16,7 +16,7 @@ describe('Unit Tests ->', function () {
         source.readyState = 2
         source.duration = 4
         source.currentTime = 0
-        layer = new CustomMedia({ startTime: 0, duration: 4, source })
+        layer = new CustomMedia({ startTime: 0, source })
 
         movie = mockMovie()
         movie.currentTime = 2

commit d275c3001eae99f66e3926ca4c2ad0a2e5f66d52

:bug: No longer set default `duration` on `options`
This fixes the bug introduced in ac3322b.

src/layer/audio-source.ts
@@ -54,11 +54,13 @@ class AudioSource extends Base {
     // Don't set as instance property
     delete options.onload
 
-    // Set a default duration so that the super constructor doesn't throw an
-    // error
-    options.duration = options.duration || 0
+    super({
+      ...options,
 
-    super(options as BaseOptions)
+      // Set a default duration so that the super constructor doesn't throw an
+      // error
+      duration: options.duration ?? 0
+    })
 
     this._initialized = false
     this._sourceStartTime = options.sourceStartTime || 0