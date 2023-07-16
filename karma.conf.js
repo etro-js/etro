fadcf6ab79086826a29a5bad46f2790aa8556af4
Caleb Sacks <16855387+clabe45@users.noreply.github.com>
2023-07-16 15:11:40 -0500

    :wrench: Decrease karma timeout
    The timeout can be increased for specific tests that generally take a
    long time.

--- karma.conf.js
+++ karma.conf.js
@@ -65,7 +65,7 @@ module.exports = function (config) {
     client: {
       captureConsole: true,
       jasmine: {
-        timeoutInterval: 20000
+        timeoutInterval: 15000
       }
     },
 
